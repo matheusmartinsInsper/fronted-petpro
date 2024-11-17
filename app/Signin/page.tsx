ebafa8d4c6daa8e3e9fa236b3cb6a7cdf343cf9e
matheusmartinsInsper <matheusmartinsInsper>
2024-11-17 19:35:32 -0300

    style: atualizar signin

--- app/Signin/page.tsx
+++ app/Signin/page.tsx
@@ -28,7 +28,7 @@ const SignupPage = () => {
       {/* Container da Esquerda */}
       <Flex flex="3.5" bg="primary.100" p={8} justifyContent="center" alignItems="center">
         <Box bgColor={"primary.300"}  bgGradient="linear(to-tr, primary.200, primary.300)"  position={"absolute"} width={["90vw","35vw"]} height={["30vh","38vh"]} top={["12vh","12vh"]} borderRadius={"lg"}></Box>
-        <Box bg="white" zIndex={"2"}  p={4} shadow="lg" borderWidth="2px" borderRadius="10px" textAlign="center" width={["80%","25%"]} height={["45%","55%"]} borderColor={"primary.100"}>
+        <Box bg="white" zIndex={"2"}  p={4} shadow="lg" borderWidth="2px" borderRadius="10px" textAlign="center" width={["80%","25%"]} height={["50%","55%"]} borderColor={"primary.100"}>
         <Flex alignItems="center" justifyContent="center">
       <Heading as="h1" size="md" mb={10} color="primary.200" display="flex" alignItems="center" marginBottom={"-5px"} opacity={"90%"}>
         Login
