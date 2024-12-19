46a118aee8e074f8654758a17a754d59feb581f4
matheusmartinsInsper <matheusmartinsInsper>
2024-12-19 14:30:53 -0300

    fixed: kes in attendance

--- app/Home/agenda/attendance/[id]/page.tsx
+++ app/Home/agenda/attendance/[id]/page.tsx
@@ -611,7 +611,7 @@ const Atendimento = () => {
               <Text fontSize="md" fontWeight="bold" mb="2" mx={4} mt={"2"}>Contra indicações</Text>
               <Box p="2" bgColor="white" overflowY="auto" maxHeight={"132px"}>
 
-               {attendanceData.pet.contraindications?.map((contraindication)=><CardContraindication contraindication={contraindication}/>)}
+               {attendanceData.pet.contraindications.map((contraindication)=><CardContraindication contraindication={contraindication}/>)}
               </Box>
             </Box>
             {/* Container do atendimento */}
